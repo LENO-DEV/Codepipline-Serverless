f5ccc950fd79f99adff54a23de66dd0dc12f4c8c
Debarshi <debopiku1122@gmail.com>
2021-04-04 11:08:21 +0530

    initial

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports.api = async event => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!'
+        message: 'Go Serverless v2.0! Your function executed successfully!'
       },
       null,
       2
