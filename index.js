6680b0df0dbb944edd285ec36df94515e0f5228e
DEBARSHI MONDAL <52108126+LENO-DEV@users.noreply.github.com>
2021-07-05 18:01:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports.api = async event => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v2.0! from dev'
+        message: 'Go Serverless v1.0! from Proton'
       },
       null,
       2
